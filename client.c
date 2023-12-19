52d6a2ce507809d32ea639535655b961be713009
moukthikjimmy <107456971+moukthikjimmy@users.noreply.github.com>
2023-12-19 21:53:09 +0530

    Add files via upload

---
+++ client.c
@@ -0,0 +1,271 @@
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/fcntl.h>
+#include <unistd.h>
+
+#define SERVER_IP "10.0.2.15" // MODIFY LATER
+#define SERVER_PORT 4500
+
+int clientSocket = -1;
+char buffer[256], buffer2[100];
+char name[100];
+
+const char* input_string(char* string, int capacity);
+void press_enter();
+void clear_screen();
+void send_and_recv();
+void load_from_file(const char* fileName);
+
+int sock_to_server();
+void bank_menu();
+
+void create_account();
+
+void login();
+void access_account();
+void withdraw();
+void deposit();
+void transfer();
+
+void credit_screen();
+void disconnection();
+
+int main()
+{
+	clientSocket = sock_to_server();
+	bank_menu(clientSocket);
+	credit_screen();
+	return 0;
+}
+
+// Function for inputting strings that may contain spaces
+const char* input_string(char* string, int capacity)
+{
+	fgets(string, capacity, stdin);
+	size_t pos = strcspn(string, "\n");
+	if(string[pos] == '\n')
+	{
+		string[pos] = '\0';
+	}
+	else
+	{
+		int ch;
+		while((ch=getchar())!='\n' && ch!=EOF)
+		{}
+	}
+	return string;
+}
+
+// Function that keeps taking input until user presses enter
+void press_enter()
+{
+	printf("Press enter to continue...");
+	input_string(buffer, 1);
+}
+
+// Function that clears terminal screen similiar to clear command
+void clear_screen()
+{
+	// \x1B[2J		-> Escape sequence to clear screen
+	// \x1B[0;0H	-> Escape sequence to move cursor to position (0, 0)
+	printf("\x1B[2J\x1B[0;0H");
+
+	// Flush standard output so that is is immediately visible
+	fflush(stdout);
+}
+
+int sock_to_server()
+{
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if ( sock < 3)
+	{
+		printf("Error: Unable to create a socket. Terminating program...\n");
+		exit(EXIT_FAILURE);
+	}
+
+	const char* ip = SERVER_IP;
+	in_port_t port = SERVER_PORT;
+
+	struct sockaddr_in sockaddr_in;
+	memset(&sockaddr_in, 0, sizeof sockaddr_in);
+	sockaddr_in.sin_family = AF_INET;
+	inet_pton(AF_INET, ip, &sockaddr_in.sin_addr.s_addr);
+	sockaddr_in.sin_port = htons(port);
+	
+	do
+	{
+		printf("\nAttempting to connect to banking server...\n");
+		sleep(1);
+		if (connect(sock, (struct sockaddr*)&sockaddr_in, sizeof sockaddr_in) == 0)
+		{
+			break;
+		}
+
+		close(sock);
+		printf("Error: Could not connect to server. Trying again in 3 seconds...\n");
+		sleep(2);
+
+	} while(1);
+	
+	printf("Successfully connected to banking server.\n\n");
+	press_enter();
+	return sock;
+}
+
+void send_and_recv(){
+	int length = strlen(buffer)+1;
+	if(write(clientSocket, buffer, length) != length 
+		|| read(clientSocket, buffer, sizeof(buffer)) <= 0)
+	{
+		disconnection();
+	}
+}
+
+void bank_menu()
+{
+	char option;
+	do
+	{
+		clear_screen();
+		printf("\nManipal Bank Menu\n");
+		printf("\nOptions\n");
+		printf("\na. Create an account\n");
+		printf("b. Login\n");
+		printf("c. Exit\n");
+		printf("\nEnter your choice: ");
+		input_string(buffer, 2);
+		option = tolower(buffer[0]);
+		switch(option)
+		{
+		case 'a':
+			create_account();
+			break;
+		case 'b':
+			login();
+			break;
+		case 'c':
+			strcpy(buffer, "quit");
+			send_and_recv();
+			break;
+		default:
+			printf("Error: Invalid option.\n\n");
+			press_enter();
+		}
+	}while(option != 'c');
+}
+
+void login()
+{
+	clear_screen();
+	printf("\nManipal Bank Account Login\n");
+	printf("\nEnter name: ");
+	input_string(name, 100);
+	sprintf(buffer, "serve %s", name);
+	send_and_recv();
+
+	if(strncmp(buffer, "Success: ", 9) == 0)
+	{
+		access_account();
+	} 
+	else
+	{
+		printf("%s\n\n", buffer);
+		press_enter();
+	}
+}
+
+void access_account()
+{
+	char option;
+	do{
+		strcpy(buffer, "query");
+		send_and_recv();
+
+		clear_screen();
+		printf("\nAccessing Manipal Bank Account\n");
+		printf("\n%s\n\n", buffer);
+		printf("Options\n");
+		printf("\na. Withdraw Money\n");
+		printf("b. Deposit Money\n");
+		printf("c. Transfer Money\n");
+		printf("d. Log Out\n");
+		printf("\nEnter your choice: ");
+		input_string(buffer, 2);
+		option = tolower(buffer[0]);
+		switch(option){
+		case 'a':
+			printf("Enter amount to withdraw: ");
+			input_string(buffer2, 20);
+			sprintf(buffer, "withdraw %s", buffer2);
+			break;
+		case 'b':
+			printf("Enter amount to deposit: ");
+			input_string(buffer2, 20);
+			sprintf(buffer, "deposit %s", buffer2);
+			break;
+		case 'c':
+			printf("Enter account name to transfer to: ");
+			input_string(buffer2, 100);
+			sprintf(buffer, "transfer %s\n", buffer2);
+			printf("\nEnter amount to transfer: ");
+			input_string(buffer2, 20);
+			strcat(buffer, buffer2);
+			break;
+		case 'd':
+			strcpy(buffer, "end");
+			break;
+		default:
+			printf("Error: Invalid choice.\n\n");
+			press_enter();
+		}
+
+		if('a' <= option && option <= 'd'){
+			send_and_recv();
+			printf("\n%s\n\n", buffer);
+			press_enter();
+		}
+	}while(option != 'd');
+}
+
+void create_account()
+{
+	clear_screen();
+	printf("\nCreating a Manipal Bank Account\n");
+	printf("\nEnter name: ");
+	input_string(name, 100);
+	sprintf(buffer, "create %s", name);
+	int length = strlen(buffer)+1;
+	if(write(clientSocket, buffer, length) != length 
+		|| read(clientSocket, buffer, sizeof(buffer)) <= 0)
+	{
+		disconnection();
+	}
+	printf("%s\n\n", buffer);
+	press_enter();
+}
+
+void load_from_file(const char* fileName)
+{
+	FILE *fp = fopen(fileName, "rb");
+	if(fp){
+		int ch;
+		while ((ch = getc(fp)) != EOF){ putchar(ch); }
+		fclose(fp);
+	}
+}
+
+void credit_screen(){
+	clear_screen();
+	load_from_file("credits.txt");
+}
+
+void disconnection(){
+	clear_screen();
+	load_from_file("disconnect.txt");
+	close(clientSocket);
+	exit(EXIT_FAILURE);
+}
---
+++ server.c
@@ -0,0 +1,500 @@
+#include <errno.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define CLIENT_PORT  4500
+#define MAX_CUSTOMERS 20
+
+typedef struct
+{
+	char	accountName[100];
+	int		isInUse;
+	float	balance;
+}Account;
+
+enum BankError{
+	NO_ACCOUNT=-10,
+	ACCOUNT_EXISTS,
+	ACCOUNT_IN_USE,
+	INSUFFICIENT_BALANCE,
+	SERVER_FULL,
+};
+
+/* Global variables used */
+static pthread_attr_t	kernelAttribute;
+static sem_t			actionLock;
+static Account			customers[MAX_CUSTOMERS];
+static pthread_mutex_t	customerMutex[MAX_CUSTOMERS];
+static pthread_mutex_t	bankMutex;
+static int				connectionCount = 0;
+
+/* Function prototypes */
+float	parse_float(const char* string);
+char*	ps(unsigned int x, char *s, char *p);
+void	abnormal_exit(const char* msg);
+void	set_iaddr(struct sockaddr_in *sockaddr, long x, unsigned int port);
+void	server_response(int sock, char* response);
+void	close_connection(int sock);
+
+int		search_account(char* accountName); 
+int		serve_account(char* accountName);
+int		create_account(char* accountName);
+
+void	periodic_action_handler(int signo, siginfo_t *ignore, void *ignore2);
+void*	periodic_action_cycle_thread(void *ignore);
+void*	session_acceptor_thread(void *ignore);
+
+int main(int argc, char **argv)
+{
+	pthread_t tid;
+	
+	// Initialize customer associated mutexes with default values
+	for (int i = 0; i < MAX_CUSTOMERS; i++)
+	{
+		if (pthread_mutex_init(&customerMutex[i], NULL) != 0)
+		{
+			abnormal_exit("pthread_mutex_init()");
+		}
+	}
+
+	// pthread_attr and mutex initializations as well as pthread_create for 
+	// session acceptor and periodic action handler
+	if (pthread_attr_init(&kernelAttribute) != 0)
+	{
+		abnormal_exit("pthread_attr_init()");
+	}
+	else if (pthread_attr_setscope(&kernelAttribute, PTHREAD_SCOPE_SYSTEM) != 0)
+	{
+		abnormal_exit("pthread_attr_setscope()");
+	}
+	else if (sem_init(&actionLock, 0, 0) != 0)
+	{
+		abnormal_exit("sem_init()");
+	}
+	else if (pthread_mutex_init(&bankMutex, NULL) != 0)
+	{
+		abnormal_exit("pthread_mutex_init()");
+	}
+	else if (pthread_create(&tid, &kernelAttribute, session_acceptor_thread, 0) != 0)
+	{
+		abnormal_exit("pthread_create()");
+	}
+	else if (pthread_create(&tid, &kernelAttribute, periodic_action_cycle_thread, 0) != 0)
+	{
+		abnormal_exit("pthread_create()");
+	}
+	else
+	{
+		printf("server is ready to receive client connections ...\n");
+		pthread_exit(0);
+	}
+}
+
+void abnormal_exit(const char* msg){
+	perror(msg);
+	exit(EXIT_FAILURE);
+}
+
+void set_iaddr(struct sockaddr_in *sockaddr, long x, unsigned int port)
+{
+	memset(sockaddr, 0, sizeof(*sockaddr));
+	sockaddr->sin_family = AF_INET;
+	sockaddr->sin_port = htons(port);
+	sockaddr->sin_addr.s_addr = htonl(x);
+}
+
+char* ps(unsigned int x, char *s, char *p)
+{
+	return x == 1 ? s : p;
+}
+
+void periodic_action_handler(int signo, siginfo_t *ignore, void *ignore2)
+{
+	if (signo == SIGALRM)
+	{
+		sem_post(&actionLock);
+	}
+}
+
+void *periodic_action_cycle_thread(void *ignore)
+{
+	/* Variables declaration, index, sigaction struct and timer */
+	int i;
+	struct sigaction action;
+	struct itimerval interval;
+
+	/* no need for any thread to join on this */
+	pthread_detach(pthread_self());
+
+	/*initialization of signal handler and timer for the 20 second account print cycle */
+	action.sa_flags = SA_SIGINFO | SA_RESTART;
+	action.sa_sigaction = periodic_action_handler;
+	sigemptyset(&action.sa_mask);
+	sigaction(SIGALRM, &action, 0);
+	/* it_value, time until next expiration
+	it_intervation, interval for periodic timer */
+	/* tv_sec seconds, tv_usec microseconds*/
+	interval.it_interval.tv_sec = 20;
+	interval.it_interval.tv_usec = 0;
+	interval.it_value.tv_sec = 20;
+	interval.it_value.tv_usec = 0;
+	// ITIMER_REAL counts down in real time
+	// which newvalue oldvalue
+	setitimer(ITIMER_REAL, &interval, 0);
+	while(1) /* infinite loop */
+	{
+		/* decrement sem count and lock a mutex to go through the bank accounts!*/
+		sem_wait(&actionLock);
+		pthread_mutex_lock(&bankMutex);
+		printf("There %s %d active %s.\nCustomers:\n", ps(connectionCount, "is", "are"),
+			connectionCount, ps(connectionCount, "connection", "connections"));
+		for (i = 0; i < MAX_CUSTOMERS; i++)
+		{
+			if (customers[i].isInUse == 1)
+			{
+				printf("IN SERVICE: ");
+			}
+			if (strcmp(customers[i].accountName, "") != 0)
+			{
+				printf("%s\n", customers[i].accountName);
+			}
+		}
+		pthread_mutex_unlock(&bankMutex);
+		/* Calling thread to relinquish CPU */
+		sched_yield();
+	}
+	return 0;
+}
+
+float parse_float(const char* string){
+	// Buffer used during float conversions
+	static char floatNumBuffer[32];
+	float num = atof(string);
+	sprintf(floatNumBuffer, "%0.2f", num);
+	num = atof(floatNumBuffer);
+	return num;
+}
+
+void server_response(int sock, char* response)
+{
+	write(sock, response, strlen(response)+1);
+	printf("%s\n", response);
+}
+
+void close_connection(int sock){
+	// Closing connection and decrementing connectionCount
+	close(sock);
+	pthread_mutex_lock(&bankMutex);
+	--connectionCount;
+	pthread_mutex_unlock(&bankMutex);
+}
+
+void* client_session_thread(void *arg)
+{
+	// main thread will not be waiting for this thread, so let us detach it
+	pthread_detach(pthread_self());
+
+	// Get mutex lock for connectionCount and increment it
+	pthread_mutex_lock(&bankMutex);
+	++connectionCount;
+	pthread_mutex_unlock(&bankMutex);
+	printf("New Client has connected!\n");
+
+	// Server socket for handling client
+	int sock = *(int *)arg;
+
+	// Holds index of current Account being handled.
+	// Initialized to NO_ACCOUNT
+	int currentIndex = NO_ACCOUNT;
+	
+	// Buffers for input and output
+	char request[256], response[256];
+
+	// Server keeps accepting input from client.
+	// When client disconnects, read() returns 0 and the server will close connection.
+	while(read(sock, request, sizeof(request)) > 0)
+	{
+		printf("\nServer received input:  %s\n", request);
+		if(currentIndex == NO_ACCOUNT)
+		{
+			if(strncmp(request, "create ", 7) == 0)
+			{
+				pthread_mutex_lock(&bankMutex);
+				int status = create_account(request+7);
+				pthread_mutex_unlock(&bankMutex);
+				switch(status){
+				case ACCOUNT_EXISTS:
+					sprintf(response, "Error: Account for %s already exists", request+7);
+					break;
+				case SERVER_FULL:
+					sprintf(response, "Error: No memory left for more accounts");
+					break;
+				
+				default:
+					sprintf(response, "Success: Account for %s was created", request+7);
+				}
+				server_response(sock, response);
+			}
+			else if(strncmp(request, "serve ", 6) == 0)
+			{
+				pthread_mutex_lock(&bankMutex);
+				int status = serve_account(request+6);
+				switch(status){
+				case NO_ACCOUNT:
+					sprintf(response, "Error: Account %s does not exist", request+6);
+					break;
+				case ACCOUNT_IN_USE:
+					sprintf(response, "Error: Account %s is already in use", request+6);
+					break;
+				
+				default:
+					currentIndex = status;
+					sprintf(response, "Success: Successfully logged into %s's account", request+6);
+					break;
+				}
+				pthread_mutex_unlock(&bankMutex);
+				server_response(sock, response);
+			}
+			else if(strncmp(request, "quit", 4) == 0)
+			{
+				break;
+			}
+			else
+			{
+				server_response(sock, "Error: Unknown command sent to base server");
+			}
+		}
+		else
+		{
+			if(strncmp(request, "query", 5) == 0)
+			{
+				pthread_mutex_lock(&customerMutex[currentIndex]);	
+				float balance = customers[currentIndex].balance;
+				pthread_mutex_unlock(&customerMutex[currentIndex]);
+				sprintf(response, "%-20s %s\n%-20s %0.2f", 
+					"Full Name: ", customers[currentIndex].accountName, 
+					"Balance in Rupees: ", balance);
+				server_response(sock, response);
+			}
+			else if(strncmp(request, "end", 3) == 0)
+			{
+				pthread_mutex_lock(&customerMutex[currentIndex]);
+				customers[currentIndex].isInUse = 0;
+				pthread_mutex_unlock(&customerMutex[currentIndex]);
+				sprintf(response, "Success: Logged out from %s's account", customers[currentIndex].accountName);
+				server_response(sock, response);
+				currentIndex = NO_ACCOUNT;
+			}
+			else if(strncmp(request, "deposit ", 8) == 0)
+			{
+				float amount = parse_float(request+8);
+				if (amount > 0)
+				{
+					pthread_mutex_lock(&customerMutex[currentIndex]);
+					customers[currentIndex].balance += amount;
+					pthread_mutex_unlock(&customerMutex[currentIndex]);
+					sprintf(response, "Success: Deposited Rs%0.2f into %s's account", 
+						amount, customers[currentIndex].accountName);
+					server_response(sock, response);
+				}
+				else
+				{
+					sprintf(response, "Error: %s sent an invalid number", 
+						customers[currentIndex].accountName);
+					server_response(sock, response);
+				}
+			}
+			else if (strncmp(request, "withdraw ", 9) == 0)
+			{
+				float amount = parse_float(request+9);
+				if (amount > 0)
+				{
+					pthread_mutex_lock(&customerMutex[currentIndex]);
+					if (customers[currentIndex].balance >= amount)
+					{
+						customers[currentIndex].balance -= amount;
+						sprintf(response, "Success: Withdrew of Rs%0.2f from %s's account", 
+							amount, customers[currentIndex].accountName);
+						server_response(sock, response);
+					}
+					else
+					{
+						sprintf(response, "Error: Cannot withdraw Rs%.2f from %s's account due to insufficient balance", 
+							amount, customers[currentIndex].accountName);
+						server_response(sock, response);
+					}
+					pthread_mutex_unlock(&customerMutex[currentIndex]);
+				}
+				else
+				{
+					sprintf(response, "Error: %s sent an invalid number", 
+						customers[currentIndex].accountName);
+					server_response(sock, response);
+				}
+			}
+			else if (strncmp(request, "transfer ", 9) == 0)
+			{
+				size_t amountPtr = strcspn(request, "\n");
+				request[amountPtr] = '\0';
+				int index = search_account(request+9);
+				if(index == NO_ACCOUNT){
+					sprintf(response, "Error: Account %s does not exist", request+9);
+					server_response(sock, response);
+				}
+				else
+				{
+					float amount = parse_float(request+amountPtr+1);
+					if (amount > 0)
+					{
+						pthread_mutex_lock(&customerMutex[currentIndex]);
+						if (customers[currentIndex].balance >= amount)
+						{
+							customers[currentIndex].balance -= amount;
+							pthread_mutex_unlock(&customerMutex[currentIndex]);
+							pthread_mutex_lock(&customerMutex[index]);
+							customers[index].balance += amount;
+							pthread_mutex_unlock(&customerMutex[index]);
+							sprintf(response, "Success: Transferred %.2f from %s to %s",
+								amount, customers[currentIndex].accountName, customers[index].accountName);
+							server_response(sock, response);
+						}
+						else
+						{
+							pthread_mutex_unlock(&customerMutex[currentIndex]);
+							sprintf(response, "Error: %s has insufficient balance for transfer of Rs%.2f", 
+								customers[currentIndex].accountName, amount);
+							server_response(sock, response);
+						}
+					}
+					else
+					{
+						sprintf(response, "Error: %s sent an invalid number", 
+							customers[currentIndex].accountName);
+						server_response(sock, response);
+					}
+				}
+			}
+			else
+			{
+				server_response(sock, "Error: Unknown command sent to login server");
+			}
+		}
+	}
+	close_connection(sock);
+	pthread_exit(0);
+}
+
+void *session_acceptor_thread(void *ignore)
+{
+	int sd;
+	int fd;
+	struct sockaddr_in addr;
+	struct sockaddr_in senderAddr;
+	socklen_t ic;
+	pthread_t tid;
+	int on = 1;
+	char *func = "session_acceptor_thread";
+
+	pthread_detach(pthread_self());					  /* no waiting on this thread ! */
+	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) /* get socket store in sd */
+	{
+		printf("socket() failed in %s()\n", func);
+		return 0;
+	}
+	else if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) == -1) /*reserve a port to bind on */
+	{
+		printf("setsockopt() failed in %s()\n", func);
+		return 0;
+	}
+	else if (set_iaddr(&addr, INADDR_ANY, CLIENT_PORT), errno = 0,
+			 bind(sd, (const struct sockaddr *)&addr, sizeof(addr)) == -1) /*bind to a port */
+	{
+		printf("bind() failed in %s() line %d errno %d\n", func, __LINE__, errno);
+		close(sd);
+		return 0;
+	}
+	else if (listen(sd, MAX_CUSTOMERS) == -1)
+	{
+		printf("listen() failed in %s() line %d errno %d\n", func, __LINE__, errno); /*listen to at most 100 connections */
+		close(sd);
+		return 0;
+	}
+	else
+	{
+		ic = sizeof(senderAddr);
+		/* accept loop, spawns a new client thread if a succesfull connection occurs , passes in the sd */
+		while ((fd = accept(sd, (struct sockaddr *)&senderAddr, &ic)) != -1)
+		{
+			if (pthread_create(&tid, &kernelAttribute, client_session_thread, &fd) != 0)
+			{
+				printf("pthread_create() failed in %s()\n", func);
+				return 0;
+			}
+			else
+			{
+				continue;
+			}
+		}
+		close(sd);
+		return 0;
+	}
+}
+
+/*
+ * Serve checks for account existence. If account is found, returns its index
+ * in the bank struct array to be used for serve !
+ */
+int search_account(char* acc_name){
+	for(int i = 0; i < MAX_CUSTOMERS; ++i)
+	{
+		if(strcmp(customers[i].accountName, acc_name) == 0)
+		{
+			return i;
+		}
+	}
+	return NO_ACCOUNT;
+}
+
+int serve_account(char *acc_name)
+{
+	int index = search_account(acc_name);
+	if(index > 0)
+	{
+		if(customers[index].isInUse == 0)
+		{
+			return index;
+		}
+		else
+		{
+			return ACCOUNT_IN_USE;
+		}
+	}
+	return index;
+}
+
+int create_account(char *acc_name)
+{
+	if(search_account(acc_name) != NO_ACCOUNT)
+	{
+		return ACCOUNT_EXISTS;
+	}
+	/* go through bank struct */
+	for (int i = 0; i < MAX_CUSTOMERS; i++)
+	{
+		if (strcmp(customers[i].accountName, "") == 0)
+		{
+			strcpy(customers[i].accountName, acc_name);
+			return i;
+		}
+	}
+	return SERVER_FULL;
+}
